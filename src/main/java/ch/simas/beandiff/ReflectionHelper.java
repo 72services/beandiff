e5262f33a7c5d6ea01ec3481df7b4477c478100d
unknown <simon.martinelli@WMO058.tgs.intern>
2015-06-05 15:06:06 +0200

    fixed reflection problem

--- src/main/java/ch/simas/beandiff/ReflectionHelper.java
+++ src/main/java/ch/simas/beandiff/ReflectionHelper.java
@@ -4,6 +4,6 @@ public class ReflectionHelper {
 
     public static boolean isPrimitiveOrStringOrWrapperOrBigDecimal(Object obj) {
         Class clazz = obj.getClass();
-        return clazz.isPrimitive() || String.class == clazz || Character.class == clazz || Boolean.class == clazz || clazz.isAssignableFrom(Number.class);
+        return clazz.isPrimitive() || String.class == clazz || Character.class == clazz || Boolean.class == clazz || Number.class.isAssignableFrom(clazz);
     }
 }
